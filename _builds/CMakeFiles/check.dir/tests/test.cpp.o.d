a51e770b42a0f0402ba42356a24e49ff901407d3
MichelsonIU815 <dmtmikel@mail.ru>
2025-04-28 14:12:05 +0300

    Add files via upload

---
+++ _builds/CMakeFiles/check.dir/tests/test.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/check.dir/tests/test.cpp.o: \
+ /home/michel/MichelsonIU815/workspace/projects/lab07/tests/test.cpp \
+ /usr/include/stdc-predef.h
---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/michel/MichelsonIU815/workspace/projects/lab07
+# Build directory: /home/michel/MichelsonIU815/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/michel/MichelsonIU815/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/michel/MichelsonIU815/workspace/projects/lab07/CMakeLists.txt;24;add_test;/home/michel/MichelsonIU815/workspace/projects/lab07/CMakeLists.txt;0;")
